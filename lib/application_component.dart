72e3e2b9564f6a9a6cb28f43912503faac452922
marska <mail@martinskafte.dk>
2018-05-18 19:34:55 +0200

    Base template, update.

---
+++ lib/application_component.dart
@@ -0,0 +1,14 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'application',
+  //styleUrls: [''],
+  directives: [],
+  //templateUrl: '',
+  template: '''
+    <h1>Dart2 Angular5 Route2</h1>
+  ''',
+)
+class ApplicationComponent {
+
+}
--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,6 @@
 import 'package:angular/angular.dart';
-import 'package:dart2_ang5_route2/app_component.template.dart' as ng;
+import 'package:dart2_ang5_route2/application_component.template.dart' as ng;
 
 void main() {
-  runApp(ng.AppComponentNgFactory);
+  runApp(ng.ApplicationComponentNgFactory);
 }
